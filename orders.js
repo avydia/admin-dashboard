864416df61eb471552c5f8c93c80cd5378c9e906
JoshJAL <JoshuaALevine3@gmail.com>
2023-03-01 18:03:08 -0500

    add button functionality, dark theme functionality, side bar functionality

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending"
+  },
+  {
+    productName: "L-Shaped Computer Desk",
+    productNumber: "75091",
+    paymentStatus: "Refunded",
+    shipping: "Declined"
+  },
+  {
+    productName: "Pure Latex Mattress Topper",
+    productNumber: "39054",
+    paymentStatus: "Due",
+    shipping: "Pending"
+  },
+  {
+    productName: "Foldable Mini Drone",
+    productNumber: "85631",
+    paymentStatus: "Paid",
+    shipping: "Delivered"
+  },
+  {
+    productName: "4K Mini Drone",
+    productNumber: "43104",
+    paymentStatus: "Refunded",
+    shipping: "Declined"
+  },
+  {
+    productName: "Pure Latex Mattress Topper",
+    productNumber: "39054",
+    paymentStatus: "Paid",
+    shipping: "Delivered"
+  },
+  {
+    productName: "L-Shaped Computer Desk",
+    productNumber: "75091",
+    paymentStatus: "Paid",
+    shipping: "Delivered"
+  },
+]
